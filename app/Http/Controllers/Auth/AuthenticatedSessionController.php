983fd74328d1168f4f09be051d175621ef4b21cf
zakariae bakkari <zakariatubtob@gmail.com>
2024-11-18 19:50:24 +0100

    initialize envirenment health-tracker laravel project

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate(); // authenticate() est une méthode de LoginRequest
+
+        $request->session()->regenerate(); // regenerate() est une méthode de Request
+
+        return redirect()->intended(route('dashboard', absolute: false));   // intended est une méthode de RedirectResponse
+    }
+
+    /**
+     * Destroy an authenticated session.(logout)
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout(); // logout() est une méthode de Auth et guard() est une méthode de Auth
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => strtolower($request->email),//stocker les emails en minuscule
+            'password' => Hash::make($request->password),// en faire le chiffrement de mot de passe
+        ]);
+
+        event(new Registered($user)); //envoyer un email de vérification
+
+        Auth::login($user); //connecter l'utilisateur
+        return redirect(route('dashboard', absolute: false)); //rediriger vers le dashboard
+    }
+}
